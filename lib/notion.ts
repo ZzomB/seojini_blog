94c837b12d62e712e915ef02abf1d21c8531135d
ZzomB <mrjbk77@gmail.com>
2025-08-28 15:16:29 +0900

    error procession added

--- lib/notion.ts
+++ lib/notion.ts
@@ -78,7 +78,7 @@ export const getPostBySlug = async (
   slug: string
 ): Promise<{
   markdown: string;
-  post: Post;
+  post: Post | null;
 }> => {
   const response = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
@@ -99,6 +99,13 @@ export const getPostBySlug = async (
       ],
     },
   });
+
+  if (!response.results[0]) {
+    return {
+      markdown: '',
+      post: null,
+    };
+  }
 
   const mdblocks = await n2m.pageToMarkdown(response.results[0].id);
   const { parent } = n2m.toMarkdownString(mdblocks);
